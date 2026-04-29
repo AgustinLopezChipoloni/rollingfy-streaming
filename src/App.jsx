44f936b843f583b867bc1e3b482e1aabc6e11b40
Diego Giménez <gimenezd15@gmail.com>
2026-04-29 20:46:25 -0300

    Arregle un error de codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
 import { useState } from "react";
 import NavBar from "./componets/NavBar";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react'
-import NavBar from "./componets/NavBar"
-import Login from "./pages/Login"
-import Registro from "./pages/Registro"
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import NavBar from "./componets/NavBar";
+import Login from "./pages/Login";
+import Registro from "./pages/Registro";
+import Home from "./pages/Home";
 import DetalleCancion from "./pages/DetalleCancion";
 function App() {
   return (
-    <>
-      <NavBar />
-    </>
     <BrowserRouter>
-      <NavBar />              
+      <NavBar />
       <Routes>
         <Route path="/Login" element={<Login />} />
-        <Route path="/Registro" element={<Registro/>} />
-        <Route path = "/" element = {<Home/>}/>
+        <Route path="/Registro" element={<Registro />} />
+        <Route path="/" element={<Home />} />
         <Route path="/detalle/:id" element={<DetalleCancion />} />
-
       </Routes>
     </BrowserRouter>
   );
