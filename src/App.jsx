9fd65c62c24c8b910eec46e8421354ab214065cb
Diego Giménez <gimenezd15@gmail.com>
2026-04-30 12:42:36 -0300

    Agregue estilos al login, agregue un h1 al componente Admin.jsx y tambien agregue la ruta del en el componente App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NavBar from "./componets/NavBar";
 import Login from "./pages/Login";
 import Registro from "./pages/Registro";
 import Home from "./pages/Home";
 import DetalleCancion from "./pages/DetalleCancion";
+import Admin from "./pages/Admin";
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="/Login" element={<Login />} />
+        <Route
+          path="/Login"
+          element={<Login setUsuarioLogueado={setUsuarioLogueado} />}
+        />
         <Route path="/Registro" element={<Registro />} />
         <Route path="/" element={<Home />} />
         <Route path="/detalle/:id" element={<DetalleCancion />} />
+        <Route path="/Admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,48 +1,39 @@
-import { Card, Col, Container, Row, Form, Button } from "react-bootstrap";
-import { FcGoogle } from "react-icons/fc";
+import { Container, Row, Card, Col, Form, Button } from "react-bootstrap";
 
 const Login = () => {
   return (
     <>
-      <Container
-        fluid
-        className="vh-100 d-flex flex-column justify-content-center"
-      >
-        <Row className="justify-content-center align-items-center">
+      <Container fluid>
+        <Row className="justify-content-center align-items-center vh-100 ">
           <Col xs={11} sm={8} md={6} lg={4}>
             <Card className="bg-dark text-light border border-light p-4 shadow">
               <Card.Body>
                 <h1 className="text-center fw-bold mb-2">Iniciar sesion</h1>
                 <p className="text-center fw-bold mb-4">Accede a tu cuenta</p>
                 <Form>
-                  {/* Email */}
-
                   <Form.Group className="mb-4">
-                    <Form.Label>Email:</Form.Label>
+                    <Form.Label className="fw-bold">Email:</Form.Label>
                     <Form.Control
                       type="email"
-                      placeholder="diegogimenez@gmail.com"
+                      placeholder="diego@gmail.com"
                       className="bg-dark text-light border-light"
                     />
                     <Form.Text className="fw-bold text-danger">
-                      Mensaje de error
+                      mensaje de error
                     </Form.Text>
                   </Form.Group>
-
-                  {/* Password */}
                   <Form.Group className="mb-3 position-relative">
-                    <Form.Label>Contraseña:</Form.Label>
+                    <Form.Label className="fw-bold">Contraseña:</Form.Label>
                     <Form.Control
-                      type="password"
-                      placeholder="Ingresa una contraseña"
+                      placeholder="Ingresa tu contraseña"
                       className="bg-dark text-light border-light pe-5"
                     />
+
                     <Form.Text className="fw-bold text-danger">
                       Mensaje de error
                     </Form.Text>
                   </Form.Group>
 
-                  {/* Boton ingresar */}
                   <Button
                     type="submit"
                     className="w-100 fw-bold bg-dark border-light"
@@ -50,9 +41,8 @@ const Login = () => {
                     Ingresar
                   </Button>
 
-                  <p className="text-center fw-bold mt-3 mb-2">o</p>
+                  <p className="text-center fw-bold mt-3">o</p>
 
-                  {/* Boton de google */}
                   <Button className="w-100 mt-2 bg-dark border-light d-flex align-items-center justify-content-center gap-2 fw-bold">
                     <FcGoogle size={20} /> Continuar con Google
                   </Button>
