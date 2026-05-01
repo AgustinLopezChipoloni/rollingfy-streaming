188f63d45947c544e67a8b6d5fcfa0b21ede432b
Milagros del Carmen Acuña <milagrosdelc49@gmail.com>
2026-05-01 11:50:01 -0300

    importé librerias de react-icons y hooks de validacion en la rama feature/registro

---
+++ src/componets/FormularioAuth.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { Card, Button, Form } from "react-bootstrap";
+import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { guardarUsuario } from "../helpers/LocalStorage";
+import "../App.css";
