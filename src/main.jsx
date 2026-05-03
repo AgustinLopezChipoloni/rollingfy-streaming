98113b8147c906a24d20f0895a7ff9e1cca7e612
Agustin Lopez <agustinlopez1112004@gmail.com>
2026-05-03 11:36:19 -0300

    se quito una dependencia

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css'
 import App from './App.jsx'
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
