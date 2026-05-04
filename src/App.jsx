7f1a85a5dffe84721e3e3901cb62de04a4405c07
joaquinsosa00 <joaquin150496@gmail.com>
2026-05-04 11:37:24 -0300

    agrego rutas protegidas al admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./componets/NavBar";
 import { AuthProvider } from "./context/AuthContext";
@@ -9,29 +8,56 @@ import DetalleCancion from "./pages/DetalleCancion";
 import Admin from "./pages/Admin";
 import Footer from "./componets/Footer";
 import FormularioCancion from "./pages/Canciones/FormularioCancion";
-import { useState, useEffect, useContext } from "react";
+import { useState } from "react";
 
-
+import RutaProtegida from "./componets/ProtectorAdmin"; 
 
 function App() {
+  const [usuarioLogueado, setUsuarioLogueado] = useState(false);
   return (
-      <AuthProvider>
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Registro" element={<Registro />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/detalle/:id" element={<DetalleCancion />} />
+    <AuthProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/Login" element={<Login setUsuarioLogueado={setUsuarioLogueado} />} />
+          <Route path="/Registro" element={<Registro />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/detalle/:id" element={<DetalleCancion />} />
 
-        <Route path="/admin" element={<Admin />} />
-        
-        <Route path="/admin/crear" element={<FormularioCancion titulo="Crear Canción" />} />
-        <Route path="/admin/editar/:id" element={<FormularioCancion titulo="Editar Canción" />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+          {/* Protegemos la tabla principal */}
+        <Route 
+          path="/admin" 
+          element={
+            <RutaProtegida usuarioLogueado={usuarioLogueado}>
+              <Admin />
+            </RutaProtegida>
+          } 
+        />
+
+        {/* Protegemos el formulario de Crear */}
+        <Route 
+          path="/admin/crear" 
+          element={
+            <RutaProtegida usuarioLogueado={usuarioLogueado}>
+              <FormularioCancion titulo="Crear Canción" />
+            </RutaProtegida>
+          } 
+        />
+
+        {/* Protegemos el formulario de Editar */}
+        <Route 
+          path="/admin/editar/:id" 
+          element={
+            <RutaProtegida usuarioLogueado={usuarioLogueado}>
+              <FormularioCancion titulo="Editar Canción" />
+            </RutaProtegida>
+          } 
+        />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </AuthProvider>
   );
 }
+
 export default App;
