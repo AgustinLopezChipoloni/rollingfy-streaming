da7ec019c356e59b1375a8acd04719be2c5e4937
matias22lucena <matias22lucena@gmail.com>
2026-05-06 20:41:37 -0300

    Corrige import de Playlist

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import DetalleCancion from "./pages/DetalleCancion";
 import Admin from "./pages/Admin";
 import Footer from "./componets/Footer";
 import FormularioCancion from "./pages/Canciones/FormularioCancion";
-import Playlist from "./pages/Playlist";
+import Playlist from "./pages/PlayList";
 import RutaProtegida from "./componets/ProtectorAdmin";
 import AdminUsuarios from "./pages/AdminUsuarios";
 import { useState, useEffect } from "react";
